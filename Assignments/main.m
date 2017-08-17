473f7110ae366f8cfc4875f0fabffc537a03f97c
AKI <a.okada3@gmail.com>
2017-08-16 22:10:00 -0700

    Initial Commit

---
+++ Assignments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignments
+//
+//  Created by 岡田暁 on 2017-08-16.
+//  Copyright © 2017 岡田暁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
