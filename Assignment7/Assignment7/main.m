62c6f2ac37abb212851817b29b6c05453db0f7b5
AKI <a.okada3@gmail.com>
2017-08-27 23:47:31 -0700

    up

--- Assignment7/Assignment7/main.m
+++ Assignment7/Assignment7/main.m
@@ -7,43 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Patient.h"
 #import "Doctor.h"
+#import "Patient.h"
 #import "Symptom.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Doctor *doctor = [Doctor newWithName:@"Dr. Alice"];
-        Patient *patient = [Patient newWithName:@"Bob"];
-        
-        // sicka
+        Doctor *doctor = [Doctor newWithName:@"Dr. Mike"];
+        Patient *patient = [Patient newWithName:@"Anne"];
         Symptom *cough = [Symptom newWithTitle:@"Cough"];
         [cough.medicines addObject:@"Benylin"];
         Symptom *fever = [Symptom newWithTitle:@"Fever"];
         [fever.medicines addObject:@"Advil"];
         [fever.medicines addObject:@"Ice"];
-        
-        // make them sick
         [patient addSympom:cough];
         [patient addSympom:fever];
-        
-        // see doctor without health card
         if (![patient visitDoctor:doctor]) {
             [patient say:@"Oh my."];
         }
-        
-        // see doctor with health card
         [patient getValidHealthCard];
         if ([patient visitDoctor:doctor]) {
             [patient say:@"OK. I need rest."];
         }
+        NSLog(@"- Few days later... -\n");
         
-        printf("- Few days later... -\n");
-        
-        // fever has gone but still conghing
+     
         [patient removeSympom:fever];
         
-        // see doctor again
+
         [patient visitDoctor:doctor];
+    }
     return 0;
 }
